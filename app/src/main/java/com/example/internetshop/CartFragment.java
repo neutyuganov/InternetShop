b1e1e4984d3b02ecd60742cdc1b886cae57af89a
utyug <utyuganovsergey55@gmail.com>
2023-04-19 10:48:49 +0600

    fragment main, cart backend. item list view for cart fragment

--- app/src/main/java/com/example/internetshop/CartFragment.java
+++ app/src/main/java/com/example/internetshop/CartFragment.java
@@ -4,15 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class CartFragment extends Fragment {
+ListView listview;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_cart, container, false);
+        View v = inflater.inflate(R.layout.fragment_cart, container, false);
+        listview = v.findViewById(R.id.listview);
+        adapter = new CustomAdapter
+        return v;
     }
 }
